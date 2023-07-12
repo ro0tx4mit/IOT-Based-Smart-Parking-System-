c95e5d460f7463f9d1937df3a7bc0e380f2a70e1
amitbisoyi <75783300+ro0tx4mit@users.noreply.github.com>
2023-07-12 19:58:21 +0530

    Update structure.sql

---
+++ structure.sql
@@ -0,0 +1,19 @@
+                      Smart Parking System
+                             |
+                +----------------------------+
+                |                            |
+          Hardware Components          Software Components
+                |                            |
+    +-----------+-----------+    +-----------+-----------+
+    |                       |    |                       |
+ Arduino Board    Ultrasonic Sensors      Central Server
+    |                       |    |                       |
+    +-----------+-----------+    +-----------+-----------+
+                |                            |
+         Occupancy Detection         Data Processing and Storage
+                |                            |
+    +-----------+-----------+    +-----------+-----------+
+    |                       |    |                       |
+   LED Indicators     Communication          Web/Mobile Application
+                        Protocol
+
